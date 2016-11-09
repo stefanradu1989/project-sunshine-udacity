780a18de0722f837bb7075287eb6bc0a0ada18d5
stefanradu1989 <stefanradu1989@gmail.com>
2016-11-09 15:05:45 +0200

    Initial commit

---
+++ app/src/main/java/com/stefan/radu/projectsunshine/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.stefan.radu.projectsunshine.utils;
+
+/**
+ * Created by rstefan on 11/9/2016.
+ */
+
+public class Constants {
+    public static final String OPEN_WEATHER_MAP_API_KEY = "62d3732ab49cda085415de002a61bc2f";
+}
